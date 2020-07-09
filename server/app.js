87fc2823192bcdd2425d47657da483b60852ad29
devDevelopmentsDesktop <devins.d@protonmail.com>
2020-07-08 21:36:22 -0300

    style

--- server/app.js
+++ server/app.js
@@ -23,17 +23,17 @@ const bakeryItemCatagories = [
     {
         productCategory: "cookies",
         productCount: 30,
-        productImg: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        productImg: "/cake_1.jpg"
     },
     {
         productCategory: "cuffins",
         productCount: 10,
-        productImg: "https://images.unsplash.com/photo-1590759668557-48e829b5529e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        productImg: "/cake_1.jpg"
     },
     {
         productCategory: "pies",
         productCount: 90,
-        productImg: "https://images.unsplash.com/photo-1587248721852-ffc60bffc129?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        productImg: "/cake_1.jpg"
     }
 ]
 
