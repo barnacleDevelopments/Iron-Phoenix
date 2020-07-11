2e796f02cbb1e0afa8e28bde540dee26c1c75c22
devDevelopmentsDesktop <devins.d@protonmail.com>
2020-07-11 15:48:35 -0300

    styles

--- public/js/nav.js
+++ public/js/nav.js
@@ -16,13 +16,37 @@ const searchBar = document.querySelector(".searchbar");
 const categoryBar = document.querySelector(".category-bar");
 
 
+$(window).scroll( function(){
+    $('.hideme').each( function(i){
+console.log($(window).scrollTop())
+console.log($(window).scrollTop() + $(this).offset().top)
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-// Bottom Scroll Animations 
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        itemDescription.classList.add("customize-expansion")
-    }
- });
+        if( bottom_of_window > bottom_of_object ){
+
+            $(this).animate({'opacity':'1'},500);
+
+        } 
+
+    }); 
+
+});
+
+$(window).scroll( function(){
+    $('.hideme').each( function(i){
+console.log($(window).scrollTop())
+console.log($(window).scrollTop() + $(this).offset().top)
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if( $(this).offset().top <= 72) {
+            $(this).animate({'opacity':'0'},500);
+        } 
+
+    }); 
+
+});
 
 
 // // HAMBURGER MENU ANIMATIONS
