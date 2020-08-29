40240129ad0b716a20822a373e1d49b055e13d0a
Devin <devins.d@protonmail.com>
2020-08-29 13:24:59 -0300

    file structure changes

--- server/app.js
+++ server/app.js
@@ -61,7 +61,10 @@ app.engine("handlebars", hbs.engine);
 app.set("views", path.join(__dirname, "../views"));
 
 //STATIC ASSETS
-app.use("/", express.static(path.join(__dirname, "../public/css")));
+app.use("/", express.static(path.join(__dirname, "../public/admin/css")));
+app.use("/", express.static(path.join(__dirname, "../public/customer/css")));
+app.use("/", express.static(path.join(__dirname, "../public/customer")));
+app.use("/", express.static(path.join(__dirname, "../public/admin")));
 app.use("/", express.static(path.join(__dirname, "../public/js")));
 app.use("/", express.static(path.join(__dirname, "../public/img")));
 app.use("/", express.static(path.join(__dirname, "../public/img/icons")));
