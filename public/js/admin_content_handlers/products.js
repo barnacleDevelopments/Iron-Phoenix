075ec77257cea48ba5ba9d6dd4281635fd0c3b67
Devin <devins.d@protonmail.com>
2020-08-29 11:55:27 -0300

    Error handling for category routes

--- public/js/admin_content_handlers/products.js
+++ public/js/admin_content_handlers/products.js
@@ -12,7 +12,7 @@
   categories.then((cats) => {
     cats.data.forEach((cat) => {
       $(".admin-category-list").append(`            
-    <li>
+    <li id="${cat._id}">
     <div class="category-header-container">
         <div class="collapsible-header">
             <div>
@@ -91,9 +91,99 @@ $("#category-create-btn").on("click", () => {
   var name = $("#add-category-input").val();
   let category = new Category();
   let newCategory = category.create(name);
-  newCategory.then((data) => {
-    $(".admin-category-list").append(`            
-    <li >
+  newCategory.then((cat) => {
+    if (!cat.err) {
+      $(".admin-category-list").append(
+        $(".admin-category-list").append(`            
+      <li>
+      <div class="category-header-container">
+          <div class="collapsible-header">
+              <div>
+                  <div class="admin-category-img">
+                      <img src="/cake_1.jpg">
+                  </div>
+                  <div>
+                      <h1>${name}</h1>
+                  </div>
+              </div>
+          </div>
+          <div class="category-dropdown-container">
+          <a class="category-dropdown-trigger">
+              <i class="material-icons">more_vert</i>
+          </a>
+          <ul class="category-dropdown" style="display: none;">
+              <li data-catid="${cat._id}" class="edit-category-btn">edit</li>
+              <li data-catid="${cat._id}" class="delete-category-btn">delete</li>
+          </ul>
+      </div>
+      </div>
+      <div class="collapsible-body">
+          <ul class=" admin-product-list">
+              <li class="admin-product">
+                  <div>
+                      <div class="admin-product-img">
+                          <img src="/cake_1.jpg">
+                      </div>
+                      <div class="admin-product-description">
+                          <h2>Product</h2>
+                          <p class="flow-text">
+                              {{elipsis "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet." 35}}</p>
+                      </div>
+                  </div>
+                  <div class="product-dropdown">
+                      <div>
+                          <a class='dropdown-trigger edit-drop-trigger' href='#'
+                              data-target='dropdown2'><i class="material-icons">more_vert</i></a>
+                          <ul id='dropdown2' class='dropdown-content'>
+                              <li><a href="#!">Addons</a></li>
+                              <li><a href="#!">Alergies</a></li>
+                              <li><a href="#!" class="edit-product-btn">Edit</a></li>
+                              <li><a href="#!" class="delete-product-btn">Delete</a></li>
+                          </ul>
+                      </div>
+                      <div class="product-price">
+                          <h3>$50.00</h3>
+                      </div>
+                  </div>
+              </li>
+  
+              <li class="product-input" style="display: none;">
+                  <div class="title_price-inputs">
+                      <input type="text" placeholder="add product title here...">
+                      <input type="text" placeholder="add price...">
+                  </div>
+  
+                  <input type="text" placeholder="add description...">
+  
+                  <div class="product-input-btns">
+                      <a class="waves-effect waves-light btn product-save-btn">save</a>
+                      <a class="waves-effect waves-light btn product-cancel-btn">cancle</a>
+                  </div>
+              </li>
+  
+              <a class="waves-effect waves-light btn add-product-btn">Add Product</a>
+      </div>
+  </li>
+  `)
+      );
+      console.log("New category created!");
+    } else {
+      console.log(cat.errMessage);
+    }
+  });
+});
+
+//edit category
+$("#save-edit-category-btn").on("click", (e) => {
+  let editMenu = document.querySelector(".edit-category-menu");
+  let catName = editMenu.children[0].value;
+  let catId = editMenu.getAttribute("data-catid");
+  let category = new Category();
+  category.update(catId, catName).then((modCat) => {
+    if (!modCat.err) {
+      $(`#${catId}`).replaceWith(
+        `            
+    <li id="${modCat._id}">
     <div class="category-header-container">
         <div class="collapsible-header">
             <div>
@@ -101,7 +191,7 @@ $("#category-create-btn").on("click", () => {
                     <img src="/cake_1.jpg">
                 </div>
                 <div>
-                    <h1>${name}</h1>
+                    <h1>${catName}</h1>
                 </div>
             </div>
         </div>
@@ -110,8 +200,8 @@ $("#category-create-btn").on("click", () => {
             <i class="material-icons">more_vert</i>
         </a>
         <ul class="category-dropdown" style="display: none;">
-            <li class="edit-category-btn">edit</li>
-            <li class="delete-category-btn">delete</li>
+            <li data-catid="${modCat._id}" class="edit-category-btn">edit</li>
+            <li data-catid="${modCat._id}" class="delete-category-btn">delete</li>
         </ul>
     </div>
     </div>
@@ -162,24 +252,23 @@ $("#category-create-btn").on("click", () => {
             <a class="waves-effect waves-light btn add-product-btn">Add Product</a>
     </div>
 </li>
-`);
-    console.log("New category created!");
+`
+      );
+    } else {
+      console.log(errMessage);
+    }
   });
-});
-
-//edit category
-$("#save-edit-category-btn").on("click", (e) => {
-  let editMenu = document.querySelector(".edit-category-menu");
-  let catName = editMenu.children[0].value;
-  let catId = editMenu.getAttribute("data-catid");
-  let category = new Category();
-  category.update(catId, catName);
 });
 
 $(".delete-confirm-btn").on("click", (e) => {
   let deleteMenu = document.querySelector(".delete-prompt");
-  console.log(deleteMenu);
   let catId = deleteMenu.getAttribute("data-catid");
   let category = new Category();
-  category.remove(catId);
+  category.remove(catId).then((data) => {
+    if (!data.err) {
+      $(`#${catId}`).remove();
+    } else {
+      console.log(data.errMessage);
+    }
+  });
 });
--- public/js/constructors/Category.js
+++ public/js/constructors/Category.js
@@ -2,161 +2,161 @@
  * Version 1
  *
  * 2020-07-01
- * 
+ *
  * Copyright 2020, Iron Phoenix, All rights reserved.
  */
 
 /**
- * A Category Class 
- * 
+ * A Category Class
+ *
  * @author Shaquille Lynch
  */
 class Category {
+  /** Constructors **/
 
-    /** Constructors **/
+  /**
+   * A constructor for Category class
+   *
+   * @param {String} name Category name
+   */
+  constructor(name) {
+    this.name = name;
+  }
 
-    /**
-     * A constructor for Category class
-     * 
-     * @param {String} name Category name
-     */
-    constructor(name){
-        this.name = name;
-    }
+  /** Methods **/
+  /**
+   * Creating the Category
+   *
+   * @param {String} name Category Name
+   * @return {Object} Created Category
+   */
+  async create(name) {
+    let sendingData = {
+        name: name,
+      },
+      receivingData = {};
 
-    /** Methods **/
-    /**
-     * Creating the Category
-     * 
-     * @param {String} name Category Name
-     * @return {Object} Created Category
-     */
-    async create(name) {
-        let sendingData = {
-            name : name
-        }, receivingData = {};
+    await fetch(`http://localhost:${port}/api/category/`, {
+      method: "post",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json", // sent request
+        Accept: "application/json", // expected data sent back
+      },
+      body: JSON.stringify(sendingData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        receivingData.data = data;
+        receivingData.err = false;
+        receivingData.errMessage = "";
+      })
+      .catch((error) => {
+        receivingData.err = true;
+        receivingData.errMessage = error;
+        console.log(error);
+      });
 
-        await fetch(`http://localhost:${port}/api/category/`, {
-            method : 'post',
-            mode:    'cors',
-            headers: {
-            'Content-Type': 'application/json',  // sent request
-            'Accept':       'application/json'   // expected data sent back
-            },
-            body: JSON.stringify(sendingData)
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            receivingData.data = data;
-            receivingData.err = false;
-            receivingData.errMessage = "";  
-        })
-        .catch((error) => { 
-            receivingData.err = true;
-            receivingData.errMessage = error;   
-            console.log(error);
-        });
+    return receivingData;
+  }
 
-        return receivingData;
-    }
+  /**
+   * Getting all the Categories
+   *
+   * @return {Object} Categories
+   */
+  async getAll() {
+    let receivingData = {};
 
-    /**
-     * Getting all the Categories
-     * 
-     * @return {Object} Categories
-     */
-    async getAll() {
-        let receivingData = {};
+    await fetch(`http://localhost:${port}/api/category/`, {
+      method: "get",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json", // sent request
+        Accept: "application/json", // expected data sent back
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        receivingData.data = data;
+        receivingData.err = false;
+        receivingData.errMessage = "";
+      })
+      .catch((error) => {
+        receivingData.err = true;
+        receivingData.errMessage = error;
+        console.log(error);
+      });
 
-        await fetch(`http://localhost:${port}/api/category/`, {
-            method : 'get',
-            mode:    'cors',
-            headers: {
-            'Content-Type': 'application/json',  // sent request
-            'Accept':       'application/json'   // expected data sent back
-            }
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            receivingData.data = data;
-            receivingData.err = false;
-            receivingData.errMessage = "";  
-        })
-        .catch((error) => { 
-            receivingData.err = true;
-            receivingData.errMessage = error;   
-            console.log(error);
-        }); 
+    return receivingData;
+  }
 
-        return receivingData;
-    }
+  /**
+   * Updating the Category
+   *
+   * @param {String} id Category Id
+   * @param {String} name Category Name
+   * @return {Object} Updated Category
+   */
+  async update(id, name) {
+    let sendingData = {
+        name: name,
+      },
+      receivingData = {};
 
+    await fetch(`http://localhost:${port}/api/category/${id}`, {
+      method: "put",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json", // sent request
+        Accept: "application/json", // expected data sent back
+      },
+      body: JSON.stringify(sendingData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        receivingData.data = data;
+        receivingData.err = false;
+        receivingData.errMessage = "";
+      })
+      .catch((error) => {
+        receivingData.err = true;
+        receivingData.errMessage = error;
+        console.log(error);
+      });
 
-    /**
-     * Updating the Category
-     * 
-     * @param {String} id Category Id
-     * @param {String} name Category Name
-     * @return {Object} Updated Category
-     */
-    async update(id, name) {
-        let sendingData = {
-            name : name
-        }, receivingData = {};
+    return receivingData;
+  }
 
-        await fetch(`http://localhost:${port}/api/category/${id}`, {
-            method : 'put',
-            mode:    'cors',
-            headers: {
-            'Content-Type': 'application/json',  // sent request
-            'Accept':       'application/json'   // expected data sent back
-            },
-            body: JSON.stringify(sendingData)
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            receivingData.data = data;
-            receivingData.err = false;
-            receivingData.errMessage = "";  
-        })
-        .catch((error) => { 
-            receivingData.err = true;
-            receivingData.errMessage = error;   
-            console.log(error);
-        });
+  /**
+   * Removing the Category
+   *
+   * @param {String} id Category Id
+   * @return {Object} Removed Category
+   */
+  async remove(id) {
+    let receivingData = {};
 
-        return receivingData;
-    }
+    await fetch(`http://localhost:${port}/api/category/${id}`, {
+      method: "delete",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json", // sent request
+        Accept: "application/json", // expected data sent back
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        receivingData.data = data;
+        receivingData.err = false;
+        receivingData.errMessage = "";
+      })
+      .catch((error) => {
+        receivingData.err = true;
+        receivingData.errMessage = error;
+        console.log(error);
+      });
 
-    /**
-     * Removing the Category
-     * 
-     * @param {String} id Category Id
-     * @return {Object} Removed Category
-     */
-    async remove(id) {
-        let receivingData = {};
-
-        await fetch(`http://localhost:${port}/api/category/${id}`, {
-            method : 'delete',
-            mode:    'cors',
-            headers: {
-            'Content-Type': 'application/json',  // sent request
-            'Accept':       'application/json'   // expected data sent back
-            }
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            receivingData.data = data;
-            receivingData.err = false;
-            receivingData.errMessage = "";  
-        })
-        .catch((error) => { 
-            receivingData.err = true;
-            receivingData.errMessage = error;   
-            console.log(error);
-        });
-
-        return receivingData;
-    }
+    return receivingData;
+  }
 }
